8ddff6490de05138fa8a161671db623f43b112de
Zakaria MOUSTAKIM <zakariamous97@gmail.com>
2025-08-24 13:12:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,127 +1,85 @@
-// const formAge = document.querySelector("#age-form");
-// const inputElements = document.querySelectorAll(".card__input");
-// const errorMessage = document.querySelectorAll(".card__errorMessage");
-// const calculAge = document.querySelector(".card__button");
-
-// const validateDay = (day) => {
-//   if (day && day > 0 && day <= 31) {
-//     return true;
-//   }
-// };
-
-// const validateMonth = (month) => {
-//   if (month && month > 0 && month <= 12) {
-//     return true;
-//   }
-// };
-
-// const validateYear = (year) => {
-//   const currentYear = new Date().getFullYear();
-//   if (year && year > 0 && year <= currentYear) {
-//     return true;
-//   }
-// };
-
-// function calculateAge(year, month, day) {
-//   const today = new Date();
-//   const birthdate = new Date(year, month - 1, day);
-//   let age = today.getFullYear() - birthdate.getFullYear();
-//   const monthDiff = today.getMonth() - birthdate.getMonth();
-
-//   if (
-//     monthDiff < 0 ||
-//     (monthDiff === 0 && today.getDate() < birthdate.getDate())
-//   ) {
-//     age--;
-//   }
-//   return age;
-// }
-
-// const isDateValid = (dayElement, monthElement, yearElement) => {
-//   let isValid = [false, false, false];
-
-//   if (!validateDay(dayElement.value)) {
-//     dayElement.classList.add("card__input--error");
-//   } else {
-//     isValid[0] = true;
-//     dayElement.classList.remove("card__input--error");
-//   }
-
-//   if (!validateMonth(monthElement.value)) {
-//     monthElement.classList.add("card__input--error");
-//   } else {
-//     isValid[1] = true;
-//     monthElement.classList.remove("card__input--error");
-//   }
-
-//   if (!validateYear(yearElement.value)) {
-//     yearElement.classList.add("card__input--error");
-//   } else {
-//     isValid[2] = true;
-//     yearElement.classList.remove("card__input--error");
-//   }
-
-//   return isValid.every((item) => item === true);
-// };
-
-// const onClickHandler = () => {
-//   const dayElement = document.querySelector(".card__input[name='day']");
-//   const monthElement = document.querySelector(".card__input[name='month']");
-//   const yearElement = document.querySelector(".card__input[name='year']");
-//   const resultElement = document.querySelector(".card__resultValue");
-
-//   if (!isDateValid(dayElement, monthElement, yearElement)) {
-//     resultElement.textContent = "--";
-//     return;
-//   }
-
-//   resultElement.textContent = calculateAge(
-//     yearElement.value,
-//     dayElement.value,
-//     monthElement.value
-//   );
-// };
-
-// inputElements.forEach((item) => {
-//   item.addEventListener("keydown", (event) => {
-//     event.key === "Enter" && onClickHandler();
-//   });
-// });
-
-// calculAge.addEventListener("click", onClickHandler);
-
-/* My Solution */
-
-//Age calculate function
-const calculateButton = document.querySelector(".card__button");
 const inputElements = document.querySelectorAll(".card__input");
+const submitButton = document.querySelector(".card__button");
 
-function calculeAge(day, month, year) {
-  const currentDate = new Date();
-  let age = currentDate.getFullYear() - year;
+const validateDay = (day) => {
+  if (day && day > 0 && day <= 31) {
+    return true;
+  }
+};
 
-  if (
-    month > currentDate.getMonth() + 1 ||
-    (month === currentDate.getMonth() + 1 && day > currentDate.getDate())
-  ) {
+const validateMonth = (month) => {
+  if (month && month > 0 && month <= 12) {
+    return true;
+  }
+};
+
+const validateYear = (year) => {
+  const currentYear = new Date().getFullYear();
+  if (year && year > 0 && year <= currentYear) {
+    return 200_00_000;
+    return true;
+  }
+};
+
+const isDateValid = (dayElement, monthElement, yearElement) => {
+  let isValid = [false, false, false];
+
+  if (!validateDay(dayElement.value)) {
+    dayElement.classList.add("card__input--error");
+  } else {
+    isValid[0] = true;
+    dayElement.classList.remove("card__input--error");
+  }
+
+  if (!validateMonth(monthElement.value)) {
+    monthElement.classList.add("card__input--error");
+  } else {
+    isValid[1] = true;
+    monthElement.classList.remove("card__input--error");
+  }
+
+  if (!validateYear(yearElement.value)) {
+    yearElement.classList.add("card__input--error");
+  } else {
+    isValid[2] = true;
+    yearElement.classList.remove("card__input--error");
+  }
+
+  return isValid.every((item) => item === true);
+};
+
+const calculateAge = (year, month, day) => {
+  const today = new Date();
+  const birthDate = new Date(year, month - 1, day);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDiff = today.getMonth() - birthDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
 
   return age;
-}
+};
 
-function clickHandler() {
-  const resultValue = document.querySelector(".card__resultValue");
+const onClickHandler = () => {
+  const dayElement = document.querySelector('.card__input[name="day"]');
+  const monthElement = document.querySelector('.card__input[name="month"]');
+  const yearElement = document.querySelector('.card__input[name="year"]');
+  const resultElement = document.querySelector(".card__resultValue");
 
-  resultValue.textContent = calculeAge(
-    inputElements[0].value,
-    inputElements[1].value,
-    inputElements[2].value
-  );
-}
+  if (!isDateValid(dayElement, monthElement, yearElement)) {
+    resultElement.textContent = "--";
+    return;
+  }
 
-inputElements.forEach((e) =>
-  e.addEventListener("keydown", (e) => {
-    e.key === "Enter" && clickHandler();
-  })
-);
+  resultElement.textContent = calculateAge(yearElement.value, monthElement.value, dayElement.value).toString();
+};
+
+// run the function when the Enter key is clicked
+inputElements.forEach((item) => {
+  item.addEventListener("keydown", (e) => {
+    e.key === "Enter" && onClickHandler();
+  });
+});
+
+submitButton.addEventListener("click", onClickHandler);
